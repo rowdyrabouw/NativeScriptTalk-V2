d16867fad12801e28b1ed4a83e8b90e7694b023b
Rowdy Rabouw <rowdy@double-r.nl>
2019-11-16 16:08:10 +0100

    Add light

--- src/app/light-controller/light-controller-component.ts
+++ src/app/light-controller/light-controller-component.ts
@@ -9,14 +9,36 @@ import { ActivatedRoute } from "@angular/router";
     styleUrls: ["./light-controller-component.css"]
 })
 export class LightControllerComponent implements OnInit {
-    serviceUUID: string = "0xcc02";
-    characteristicUUID: string = "0xee03";
+    serviceUUID: string = "cc02";
+    characteristicUUID: string = "ee03";
     deviceUUID: string = "";
 
-    constructor(private route: ActivatedRoute) {}
+    constructor(private route: ActivatedRoute, private colorPicker: ColorPicker) {}
 
     ngOnInit() {
         this.deviceUUID = this.route.snapshot.params["UUID"];
+        this.writeColor("#09CE19");
+    }
+
+    showRainbow() {
+        console.log("showRainbow");
+    }
+
+    writeColor(color) {
+        bluetooth.writeWithoutResponse({
+            serviceUUID: this.serviceUUID,
+            characteristicUUID: this.characteristicUUID,
+            peripheralUUID: this.deviceUUID,
+            value: this.hexToRgbToValue(color)
+        });
+    }
+
+    hexToRgbToValue(color) {
+        var c = parseInt(color.substring(1), 16);
+        var r = (c >> 16) & 255;
+        var g = (c >> 8) & 255;
+        var b = c & 255;
+        return new Uint8Array([0x01, g, 0x01, 0x00, 0x01, b, 0x01, r, 0x01, 0x00]);
     }
 
     rgbToHex(red, green, blue) {
